0ab6800da36bfc3e8b963abb9e8619e863ce71e1
novelkhan <novel4004@gmail.com>
2024-03-26 09:35:53 +0600

    Add project files.

---
+++ AspNetCoreCrudUsingStoredProcedures/Models/BookViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreCrudUsingStoredProcedures.Models
+{
+    public class BookViewModel
+    {
+        [Key]
+        public int bookId { get; set; }
+        public string title { get; set; }
+        public string author { get; set; }
+        public int price { get; set; }
+    }
+}
